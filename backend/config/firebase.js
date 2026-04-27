41c40c57113e637410b49c69547f51e19c0a9980
Pratham 98max <shelken435@gmail.com>
2026-04-27 20:52:28 +0530

    fixed firebase env config

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,8 +1,13 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const admin = require("firebase-admin");
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
